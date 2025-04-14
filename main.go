360c5b21ecf492d82a8ac7bdf7a214f27cc7bca5
aryapuja <aryapuja2103@gmail.com>
2025-04-14 20:53:51 +0700

    Edit Config for deployment

--- main.go
+++ main.go
@@ -11,17 +11,17 @@ import (
 	_ "go-bookcase/docs" // Mengimpor package docs untuk Swagger
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 func main() {
 	// Memuat file .env
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatalf("Error loading .env file")
+	// }
 
 	db, err := config.SetupDatabase()
 	if err != nil {
@@ -34,7 +34,6 @@ func main() {
 	// Setup Swagger documentation
 	// Menyajikan Swagger UI dengan ginSwagger.Default
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-
 
 	// Setup routes
 	routes.SetupRoutes(r)
